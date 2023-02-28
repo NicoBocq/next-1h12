66d0bc2bf4d61b01d6da671cc468b7c74f591a47
NicoBocq <nicolas.fow@gmail.com>
2023-02-28 10:18:26 +0100

    :wrench: Import types from Supabase, add supabase client typing

--- types/index.ts
+++ types/index.ts
@@ -1,97 +1,152 @@
-export type ProjectItem = {
-  id: string
-  title: string
-  description: string
-  url: string
-  stack: {
-    name: string
-    id: string
-  }[]
-  hp: boolean
-}
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | {[key: string]: Json}
+  | Json[]
 
-export type ProjectRawResponse = {
-  id: string
-  properties: {
-    Title: {
-      title: {
-        text: {
-          content: string
+export interface Database {
+  public: {
+    Tables: {
+      page: {
+        Row: {
+          created_at: string | null
+          description: string | null
+          id: number
+          slug: string | null
+          title: string | null
         }
-      }[]
-    }
-    Description: {
-      rich_text: {
-        plain_text: string
-      }[]
-    }
-    Stack: {
-      multi_select: {
-        name: string
-        id: string
-      }[]
-    }
-
-    URL: {
-      url: string
-    }
-    HP: {
-      checkbox: boolean
-    }
-  }
-}
-
-export type ExperienceRawResponse = {
-  id: string
-  properties: {
-    Title: {
-      title: {
-        text: {
-          content: string
+        Insert: {
+          created_at?: string | null
+          description?: string | null
+          id?: number
+          slug?: string | null
+          title?: string | null
         }
-      }[]
+        Update: {
+          created_at?: string | null
+          description?: string | null
+          id?: number
+          slug?: string | null
+          title?: string | null
+        }
+      }
+      project: {
+        Row: {
+          cover: string | null
+          created_at: string | null
+          description: string | null
+          git_url: string | null
+          id: number
+          slug: string | null
+          title: string | null
+          url: string | null
+        }
+        Insert: {
+          cover?: string | null
+          created_at?: string | null
+          description?: string | null
+          git_url?: string | null
+          id?: number
+          slug?: string | null
+          title?: string | null
+          url?: string | null
+        }
+        Update: {
+          cover?: string | null
+          created_at?: string | null
+          description?: string | null
+          git_url?: string | null
+          id?: number
+          slug?: string | null
+          title?: string | null
+          url?: string | null
+        }
+      }
+      project_stacks: {
+        Row: {
+          element_id: number
+          id: number
+          stack_id: number
+        }
+        Insert: {
+          element_id: number
+          id?: number
+          stack_id: number
+        }
+        Update: {
+          element_id?: number
+          id?: number
+          stack_id?: number
+        }
+      }
+      stack: {
+        Row: {
+          id: number
+          name: string | null
+        }
+        Insert: {
+          id?: number
+          name?: string | null
+        }
+        Update: {
+          id?: number
+          name?: string | null
+        }
+      }
+      work: {
+        Row: {
+          company: string | null
+          description: string | null
+          id: number
+          period: string | null
+          title: string | null
+        }
+        Insert: {
+          company?: string | null
+          description?: string | null
+          id?: number
+          period?: string | null
+          title?: string | null
+        }
+        Update: {
+          company?: string | null
+          description?: string | null
+          id?: number
+          period?: string | null
+          title?: string | null
+        }
+      }
+      work_stacks: {
+        Row: {
+          id: number
+          stack_id: number
+          work_id: number
+        }
+        Insert: {
+          id?: number
+          stack_id: number
+          work_id: number
+        }
+        Update: {
+          id?: number
+          stack_id?: number
+          work_id?: number
+        }
+      }
     }
-    Description: {
-      rich_text: {
-        plain_text: string
-      }[]
+    Views: {
+      [_ in never]: never
     }
-    Stack: {
-      multi_select: {
-        name: string
-        id: string
-      }[]
+    Functions: {
+      [_ in never]: never
     }
-    Company: {
-      rich_text: {
-        plain_text: string
-      }[]
+    Enums: {
+      [_ in never]: never
     }
-    Period: {
-      rich_text: {
-        plain_text: string
-      }[]
+    CompositeTypes: {
+      [_ in never]: never
     }
-  }
-}
-
-export type ExperienceItem = {
-  id: string
-  title: string
-  description: string
-  stack: {
-    name: string
-    id: string
-  }[]
-  company: string
-  period: string
-}
-
-export type Pages = 'home' | 'sideprojects' | 'work'
-
-export type ConfigDictType = {
-  [key: string]: {
-    database_id: string
-    formater: Function
   }
 }
